c6bbb3d821ac58d3e7ca79ba304bf5cf614de3da
thunder04 <70254851+thunder04@users.noreply.github.com>
2021-12-05 17:53:34 +0200

    chore(day2): Performance improvements

--- rust/src/year_2021/day3.rs
+++ rust/src/year_2021/day3.rs
@@ -1,24 +1,23 @@
-#[allow(dead_code)]
 pub fn part1(input: &str) -> i64 {
     let mut epsilon: i64 = 0;
     let mut gamma: i64 = 0;
 
-    (0..12).rev().for_each(|i| {
+    (0..12).for_each(|i| {
         let mut zeros = 0;
         let mut ones = 0;
 
         for line in input.lines() {
-            if i32::from_str_radix(line, 2).unwrap() & (1 << i) == 0 {
-                zeros += 1
-            } else {
-                ones += 1
+            match line.as_bytes()[i] {
+                48 => zeros += 1,
+                49 => ones += 1,
+                _ => {}
             }
         }
 
-        if ones >= zeros {
-            gamma |= 1 << i;
+        if ones > zeros {
+            gamma |= 1 << (11 - i)
         } else {
-            epsilon |= 1 << i;
+            epsilon |= 1 << (11 - i)
         }
     });
 
